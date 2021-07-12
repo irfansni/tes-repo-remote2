56a2b3e8e13eeb61131ab3c39ced4607a75f94d0
Irfan Yose <irfanyose@gmail.com>
2021-07-12 07:48:50 +0700

    Testing commit ke 2

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>tes rmeote dari lokal ke origin</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellendus, rem perspiciatis in explicabo corporis velit est dolores dolorem cumque vero at enim, sunt voluptate ut officiis ipsam natus aperiam.</p>
 </body>
 </html>
